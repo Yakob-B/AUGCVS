83c9ae0f442625a458ee78637998bb2b866ae36e
Yakob-B <yakobatechno@gmail.com>
2025-11-29 23:01:21 +0300

    Fixemail timeout

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,7 +9,14 @@ const sendEmail = async (options) => {
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
-        }
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
+        },
+        // Force IPv4 to avoid IPv6 issues on some cloud providers
+        family: 4,
+        connectionTimeout: 10000 // 10 seconds
     });
 
     console.log(`Attempting to send email using host: ${process.env.SMTP_HOST}, port: ${process.env.SMTP_PORT}, secure: ${process.env.SMTP_PORT == 465}`);
